099cabc3eead951177bfe1e29baf050abb9e49d3
RobertPham0327 <s3926681@rmit.edu.vn>
2024-07-05 22:36:43 +0700

    update product controller

--- modules/product/controllers/ProductController.php
+++ modules/product/controllers/ProductController.php
@@ -4,9 +4,25 @@ namespace app\modules\controllers;
 
 use app\controllers\Controller;
 use app\modules\models\Product;
+use Yii;
+use yii\web\NotFoundHttpException;
+use yii\web\Response;
 
 class ProductController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'contentNegotiator' => [
+                'class' => \yii\filters\ContentNegotiator::class,
+                'only' => ['index', 'view', 'create', 'update', 'delete', 'test-many-query'],
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+                ],
+            ],
+        ];
+    }
+
     public function actionIndex()
     {
         return Product::find()->all();
@@ -25,4 +41,55 @@ class ProductController extends Controller
         }
         return $countCategoryProduct > 4;
     }
+
+    public function actionView($id)
+    {
+        return $this->findModel($id);
+    }
+
+    public function actionCreate()
+    {
+        $model = new Product();
+        $response = Yii::$app->response;
+
+        if ($model->load(Yii::$app->request->post(), '') && $model->save()) {
+            $response->statusCode = 201; 
+            return $model;
+        }
+
+        $response->statusCode = 400;
+        return ['errors' => $model->errors];
+    }
+
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+        $response = Yii::$app->response;
+
+        if ($model->load(Yii::$app->request->post(), '') && $model->save()) {
+            return $model;
+        }
+
+        $response->statusCode = 400; 
+        return ['errors' => $model->errors];
+    }
+
+    public function actionDelete($id)
+    {
+        $model = $this->findModel($id);
+        $model->delete();
+
+        Yii::$app->response->statusCode = 204; 
+        return ['message' => 'Product deleted'];
+    }
+
+    protected function findModel($id)
+    {
+        if (($model = Product::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested product does not exist.');
+    }
+
 }
